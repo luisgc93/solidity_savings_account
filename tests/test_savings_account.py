c060d7069154ad4486f341cf82bb750fe0719457
luisgc93 <luisgc93@gmail.com>
2021-11-25 20:35:23 -0500

    implement additional tests

--- tests/test_savings_account.py
+++ tests/test_savings_account.py
@@ -1,5 +1,7 @@
 import pytest
 import time
+from freezegun import freeze_time
+from datetime import datetime, timedelta
 from brownie import network
 from brownie.exceptions import VirtualMachineError
 from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
@@ -10,6 +12,7 @@ def test_cannot_deploy_if_target_date_is_in_the_past():
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip()
     target_date_in_unix = 1606346617
+    
     with pytest.raises(VirtualMachineError):
         deploy_savings_account(target_date_in_unix)
 
@@ -17,6 +20,7 @@ def test_can_deploy_if_target_date_is_in_the_future():
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip()
     target_date_in_unix = time.time() + 1000
+    
     savings_account_contract = deploy_savings_account(target_date_in_unix)
     
     assert savings_account_contract.owner() == get_account().address
@@ -28,15 +32,51 @@ def test_updates_contract_balance_and_eth_break_even_price_when_funded():
     account = get_account()
     savings_account_contract = deploy_savings_account(target_date_in_unix)
     
-    account.transfer(savings_account_contract.address, "1 ether")
+    tx = account.transfer(savings_account_contract.address, "1 ether")
+    tx.wait(1)
+    
     assert savings_account_contract.balance() == 10**18
 
 
-def test_cannot_withdraw_if_target_date_is_in_the_future_and_current_eth_price_is_lt_break_even_price():
-    pass
+def test_cannot_withdraw_if_target_date_is_in_the_future_and_current_eth_price_is_not_gt_break_even_price():
+    pytest.skip()
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    target_date_in_unix = time.time() + 1000
+    account = get_account()
+    savings_account_contract = deploy_savings_account(target_date_in_unix)
+    
+    tx = account.transfer(savings_account_contract.address, "1 ether")
+    tx.wait(1)
+    
+    with pytest.raises(VirtualMachineError):
+        savings_account_contract.withdraw()
 
 def test_can_withdraw_if_target_date_is_in_the_past():
-    pass
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    target_date_in_unix = time.time() + 1000
+    account = get_account()
+    savings_account_contract = deploy_savings_account(target_date_in_unix)
+    
+    tx = account.transfer(savings_account_contract.address, "1 ether")
+    tx.wait(1)
+
+    future_date = datetime.now() + timedelta(days=30)
+    with freeze_time(future_date.isoformat()):
+        savings_account_contract.withdraw()
+
+        assert savings_account_contract.balance() == 0
 
 def test_can_withdraw_if_current_eth_price_is_gt_break_even_price():
-    pass
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    target_date_in_unix = time.time() + 1000
+    account = get_account()
+    savings_account_contract = deploy_savings_account(target_date_in_unix)
+    
+    tx = account.transfer(savings_account_contract.address, "1 ether")
+    tx.wait(1)
+    savings_account_contract.withdraw()
+
+    assert savings_account_contract.balance() == 0
