49d47429b73a94221d855fa4ea8811d668723a01
luisgc93 <luisgc93@gmail.com>
2021-11-26 11:25:49 -0500

    fix integration tests

--- tests/test_savings_account_integration.py
+++ tests/test_savings_account_integration.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+from freezegun.api import _freeze_time
 import pytest
 import time
 from brownie import network
@@ -8,11 +10,17 @@ from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
 def test_transfers_and_withdraws_funds_when_conditions_are_valid():
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip()
-    target_date_timestamp = time.time()
+    target_date = datetime.now() + timedelta(days=15)
     account = get_account()
-    savings_account_contract = deploy_savings_account(target_date_timestamp)
+    savings_account_contract = deploy_savings_account(target_date.timestamp())
 
     account.transfer(savings_account_contract.address, "1 gwei")
     time.sleep(60)
 
-    assert savings_account_contract.balance() == 1
+    assert savings_account_contract.balance() == 1000_000_000
+
+    future_date = target_date + timedelta(days=1)
+    with _freeze_time(future_date.isoformat()):
+        savings_account_contract.withdraw()
+
+        assert savings_account_contract.balance() == 0
