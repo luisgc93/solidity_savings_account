4f11cf960442e19804b2319952be301e0f18463f
luisgc93 <luisgc93@gmail.com>
2021-11-25 21:46:13 -0500

    make last test pass

--- tests/test_savings_account.py
+++ tests/test_savings_account.py
@@ -4,7 +4,7 @@ from freezegun import freeze_time
 from datetime import datetime, timedelta
 from brownie import network
 from brownie.exceptions import VirtualMachineError
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account, get_contract
 from scripts.deploy import deploy_savings_account
 
 
@@ -39,7 +39,6 @@ def test_updates_contract_balance_and_eth_break_even_price_when_funded():
 
 
 def test_cannot_withdraw_if_target_date_is_in_the_future_and_current_eth_price_is_not_gt_break_even_price():
-    pytest.skip()
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip()
     target_date_in_unix = time.time() + 1000
@@ -48,8 +47,11 @@ def test_cannot_withdraw_if_target_date_is_in_the_future_and_current_eth_price_i
     
     tx = account.transfer(savings_account_contract.address, "1 ether")
     tx.wait(1)
+
+    new_price  = savings_account_contract.getLatestPrice() - 1000
+    get_contract("eth_usd_price_feed").updateAnswer(new_price)
     
-    with pytest.raises(VirtualMachineError):
+    with pytest.raises(VirtualMachineError) as e:
         savings_account_contract.withdraw()
 
 def test_can_withdraw_if_target_date_is_in_the_past():
