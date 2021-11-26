1d3bb1694ccf91394a33f85c8e99e6a00a47695d
luisgc93 <luisgc93@gmail.com>
2021-11-25 23:14:18 -0500

    linting

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,18 +1,27 @@
 from datetime import datetime, timedelta
 from brownie import config, network, MockV3Aggregator, SavingsAccount
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, deploy_mocks, get_account
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    deploy_mocks,
+    get_account,
+)
+
 
 def deploy_savings_account(target_date_timestamp):
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         deploy_mocks()
         price_feed_address = MockV3Aggregator[-1].address
     else:
-        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
 
+    account = get_account()
+    print(f"Deploying to {account.address}!")
     savings_account = SavingsAccount.deploy(
         price_feed_address,
         target_date_timestamp,
-        {"from": get_account()}
+        {"from": account},
     )
 
     print(f"Contract deployed to {savings_account.address}")
