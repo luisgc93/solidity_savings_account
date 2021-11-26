02624ff57585abcfb231b8b90ce5ccb485613112
luisgc93 <luisgc93@gmail.com>
2021-11-25 21:52:24 -0500

    remove redundant import

--- contracts/SavingsAccount.sol
+++ contracts/SavingsAccount.sol
@@ -3,7 +3,6 @@
 pragma solidity >=0.8 <0.9.0;
 
 import "./PriceFeedConsumer.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 
 contract SavingsAccount is PriceFeedConsumer {
